e06b97f4850c6e3ae694aea29658b8f850bccecf
sathis <sathishbabu462@gmail.com>
2024-01-31 23:00:03 +0530

    updated card

---
+++ src/Componet/Card.jsx
@@ -0,0 +1,24 @@
+export default () => (
+    <div className="birthdayCard">
+      <div className="cardFront">
+        <h3 className="happy">HAPPY BIRTHDAY HR!</h3>
+        <div className="balloons">
+          <div className="balloonOne" />
+          <div className="balloonTwo" />
+          <div className="balloonThree" />
+          <div className="balloonFour" />
+        </div>
+      </div>
+      <div className="cardInside">
+        <h3 className="back">HAPPY BIRTHDAY HR!</h3>
+        <p>Dear HR,</p>
+        <p>
+          Happy birthday!! I hope your day is filled with lots of love and
+          laughter! May all of your birthday wishes come true and this hole year lot of sweet memories filled with your dearest persons
+         and be com and be happy any way happy birthday HR Mamta
+                  </p>
+        <p className="name">Sathya Deva</p>
+      </div>
+    </div>
+  );
+  
